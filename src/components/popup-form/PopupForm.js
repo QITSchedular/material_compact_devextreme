d17cc95858b74f340326e0c91a71c583237bfb0b
DhruviRana <dhruvirana987@gmail.com>
2023-07-26 20:01:42 +0530

    sucess msg inside popup body

--- src/components/popup-form/PopupForm.js
+++ src/components/popup-form/PopupForm.js
@@ -1,28 +1,22 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import "./PopupForm.scss"
+import React, { useContext, useRef, useState } from "react";
+import "./PopupForm.scss";
 import {
-    Button,
-    CheckBox,
     Form,
-    LoadPanel,
     Popup,
     ScrollView,
-    SelectBox,
 } from "devextreme-react";
-import { ButtonItem, GroupItem, Item, SimpleItem } from "devextreme-react/form";
+import { Button } from 'devextreme-react/button';
+import { ButtonItem, GroupItem, Item } from "devextreme-react/form";
 import { AppContext } from "../../contexts/dataContext";
 import { RequiredRule } from "devextreme-react/form";
-// import "./PopupForm.scss"
-// import { RequiredRule } from "devextreme-react/data-grid";
-// import { GroupItem, RequiredRule } from "devextreme-react/data-grid";
+
+import success from '../../assets/images/success.gif';
+
 
 const PopupForm = ({
     title,
     field,
-    // isPopupVisible
 }) => {
-
-
     const formPopup = useRef(null);
 
     // Button - input options
@@ -39,7 +33,6 @@ const PopupForm = ({
         },
     };
 
-
     const dropdownOptions = {
         stylingMode: ["outlined"],
         labelModes: ["static"],
@@ -53,6 +46,8 @@ const PopupForm = ({
         disabled: true,
         value: 10,
     };
+
+    const [displayState, setdisplayState] = useState(true);
 
     const { isCommonPopupVisible, closeCommonPopup } = useContext(AppContext);
     const [popupVisible, setCommonPopupVisible] = useState(true);
@@ -70,14 +65,19 @@ const PopupForm = ({
         e.preventDefault();
         const form = formPopup.current.instance;
         form.resetValues();
+        setdisplayState(false);
+
+        setInterval(() => {
+            setdisplayState(true);
+        }, 1800);
     };
+
     var checkBoxOptions = {
-        text: 'Locked'
-      };
-    
+        text: "Locked",
+    };
+
     return (
         <>
-
             <Popup
                 maxWidth={850}
                 height={500}
@@ -90,89 +90,92 @@ const PopupForm = ({
                 container=".dx-viewport"
                 className="item-master-popup-container"
             >
+                <ScrollView ScrollView width="100%" height="100%">
+                    <div className={"dx-card content-block responsive-paddings pop-content-container"}>
+                        {
+                            (displayState)
+                                ?
+                                <>
+                                    <div className="popup-header">
+                                        <h4>{title}</h4>
+                                        <Button icon="close" onClick={handleClosePopUp} />
+                                    </div>
 
-
-
-                <ScrollView ScrollView width="100%" height="90%">
-                    <div
-                        className={
-                            "dx-card content-block responsive-paddings pop-content-container"
+                                    <form id="popupform" onSubmit={handleSubmit}>
+                                        <Form
+                                            ref={formPopup}
+                                            labelLocation={"top"}
+                                            id="form"
+                                            labelMode="floating"
+                                            style={{ boxShadow: "none" }}
+                                            className="form-element">
+                                            <GroupItem>
+                                                {field.map((item, index) => {
+                                                    checkBoxOptions = {
+                                                        text: item.label,
+                                                    };
+                                                    const key = item.feildType;
+                                                    const isValidate = item.isValidate;
+                                                    const label = item.label;
+                                                    if (key === "dxCheckBox") {
+                                                        return (
+                                                            <Item
+                                                                dataField="  "
+                                                                editorOptions={checkBoxOptions}
+                                                                editorType={key}
+                                                            >
+                                                                {isValidate && (
+                                                                    <RequiredRule message={label + " is Required"} />
+                                                                )}
+                                                            </Item>
+                                                        );
+                                                    } else {
+                                                        return (
+                                                            <Item
+                                                                dataField={label}
+                                                                editorOptions={textEditorOptions}
+                                                                editorType={key}
+                                                            >
+                                                                {isValidate && (
+                                                                    <RequiredRule message={label + " is Required"} />
+                                                                )}
+                                                            </Item>
+                                                        );
+                                                    }
+                                                })}
+                                            </GroupItem>
+                                            <Item
+                                                itemType="group"
+                                                colCount={2}
+                                                colSpan={1}
+                                                cssClass={"bottom-button-section submit-section"}
+                                            >
+                                                <ButtonItem
+                                                    buttonOptions={cancelButtonOptions}
+                                                    horizontalAlignment="right"
+                                                    cssClass={"popup-btn-cancel"}
+                                                // onClick={handleClosePopUp}
+                                                />
+                                                <ButtonItem
+                                                    buttonOptions={saveButtonOptions}
+                                                    horizontalAlignment="right"
+                                                    useSubmitBehavior={true}
+                                                    cssClass={"popup-btn-save"}
+                                                />
+                                            </Item>
+                                        </Form>
+                                    </form>
+                                </>
+                                :
+                                <div className="successAlert">
+                                    <span className="title">{title} Successful</span>
+                                    <img src={success} alt="success" height="200" width="200" />
+                                </div>
                         }
-                    >
-                        <div className="popup-header">
-                            <h4>{title}</h4>
-                            <Button icon="close"
-                                onClick={handleClosePopUp}
-                            />
-                        </div>
-
-                        <form id="popupform"
-                            onSubmit={handleSubmit}
-                        >
-                            <Form
-                                ref={formPopup}
-                                labelLocation={"top"}
-                                id="form"
-                                labelMode="floating"
-                                style={{ boxShadow: "none" }}
-                                className="form-element"
-                            >
-                                <GroupItem>
-                                    
-                                    {field.map((item, index) => {
-                                        checkBoxOptions = {
-                                            text: item.label
-                                          };
-                                        const key = item.feildType;
-                                        const isValidate = item.isValidate;
-                                        const label = item.label;
-                                        if(key=="dxCheckBox"){
-                                            return (
-                                                <Item
-                                                    dataField="  "
-                                                    editorOptions={checkBoxOptions}
-                                                    editorType={key}
-                                                >
-                                                    {isValidate && <RequiredRule message={label + " is Required"} />}
-                                                </Item>
-                                            )
-                                        }else{
-                                            return (
-                                                <Item
-                                                    dataField={label}
-                                                    editorOptions={textEditorOptions}
-                                                    editorType={key}
-                                                >
-                                                    {isValidate && <RequiredRule message={label + " is Required"} />}
-                                                </Item>
-                                            )
-                                        }
-                                        
-                                    })}
-                                </GroupItem>
-                                <Item
-                                    itemType="group"
-                                    colCount={2}
-                                    colSpan={1}
-                                    cssClass={"bottom-button-section submit-section"}
-                                >
-                                    <ButtonItem buttonOptions={cancelButtonOptions}
-                                        horizontalAlignment="right"
-                                        cssClass={"popup-btn-cancel"}
-                                    />
-                                    <ButtonItem buttonOptions={saveButtonOptions}
-                                        horizontalAlignment="right"
-                                        useSubmitBehavior={true}
-                                        cssClass={"popup-btn-save"}
-                                    />
-                                </Item>
-                            </Form>
-                        </form>
                     </div>
                 </ScrollView>
 
-
-            </Popup >
+            </Popup>
         </>
     );
 };
