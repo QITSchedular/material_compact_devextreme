15f53c39836093aaec9608bce01687428eb22467
unknown <poojangheewala7@gmail.com>
2023-08-10 17:56:13 +0530

    Do not merge this commit

--- src/pages/purchases/gate-in/old-entry.js
+++ src/pages/purchases/gate-in/old-entry.js
@@ -78,8 +78,6 @@ function OldEntryComponent() {
 
                 <Editing
                     mode="row"
-                    allowDeleting
-                    allowUpdating
                     selectTextOnEditStart={true}
 
                 />
