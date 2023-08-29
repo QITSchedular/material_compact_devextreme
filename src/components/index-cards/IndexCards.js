0d5c5c45a5716172840cb50f2dfdfe69069e4d70
QITSchedular <qitschedular2023@gmail.com>
2023-08-29 17:16:28 +0530

    test commit, do not push this

--- src/components/index-cards/IndexCards.js
+++ src/components/index-cards/IndexCards.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import { navigation } from '../../app-navigation';
-import Card from '../../components/card/Card';
+import React from "react";
+import { navigation } from "../../app-navigation";
+import Card from "../../components/card/Card";
 
 export default function IndexCards({ path }) {
-    let navobj = {}, arr = [];
+  let navobj = {},
+    arr = [];
 
-    navigation.forEach((value1, key) => {
-        Object.keys(value1).forEach((value2, key) => {
-            if (value2 === 'items') {
-                arr = value1[value2];
-                navobj[value1['path']] = arr;
-            }
-        })
+  navigation.forEach((value1, key) => {
+    Object.keys(value1).forEach((value2, key) => {
+      if (value2 === "items") {
+        arr = value1[value2];
+        navobj[value1["path"]] = arr;
+      }
     });
+  });
 
-    return (
-        <React.Fragment>
-            <div className="grid-container">
-                {
-                    navobj[path].map((value, key) => (
-                        <>
-                            {(typeof value['icon'] === "object") ? console.log(value['icon'].dark) : console.log(value['icon'])}
-                            <Card
-                                title={value['text']}
-                                description={"Here is the description for the particular card"}
-                                icon={(typeof value['icon'] === "object") ? value['icon'].dark : value['icon']}
-                                path={value['path']}
-                            />
-                        </>
-                    ))}
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <div className="grid-container">
+        {navobj[path].map((value, key) => (
+          <>
+            <Card
+              title={value["text"]}
+              description={"Here is the description for the particular card"}
+              icon={
+                typeof value["icon"] === "object"
+                  ? value["icon"].dark
+                  : value["icon"]
+              }
+              path={value["path"]}
+            />
+          </>
+        ))}
+      </div>
+    </React.Fragment>
+  );
 }
