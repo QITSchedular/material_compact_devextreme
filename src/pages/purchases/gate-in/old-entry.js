17e9cb0d80b9c287b0807f16c806afc7ceabb952
DhruviRana <dhruvirana987@gmail.com>
2023-10-10 15:41:28 +0530

    implement skeleton loader in GateIn old entry.js page 10-oct-2023 3:41

--- src/pages/purchases/gate-in/old-entry.js
+++ src/pages/purchases/gate-in/old-entry.js
@@ -10,7 +10,8 @@ import DataGrid, {
   Button,
 } from "devextreme-react/data-grid";
 import { GateInList } from "../../../utils/gate-in-purchase";
-
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 import { toast } from "react-toastify";
 import "./gate-in-styles.scss";
@@ -42,7 +43,9 @@ function OldEntryComponent() {
       const transPortersData = await GateInList();
       if (transPortersData.length > 0) {
         setTransporterDataSource(transPortersData);
-        setLoading(false);
+        setTimeout(() => {
+          setLoading(false);
+        }, 5000);
       } else {
         // No data found, display the error message using your custom toaster function
         console.log("no data");
@@ -59,67 +62,80 @@ function OldEntryComponent() {
 
   return (
     <>
-    {console.log("loading",loading)}
-    {loading && <LoadPanel  visible={true} />}
-    
-     <DataGrid
-        id="data-grid-container-local"
-        dataSource={transporterDataSource}
-        keyExpr={"srNo"}
-        showBorders={true}
-        focusedRowEnabled={true}
-        defaultFocusedRowIndex={0}
-        columnAutoWidth={true}
-        remoteOperations={true}
-        height={400}
-        className="gate-in-data-grid"
-      >
-        <SearchPanel visible={true} width={190} className={"search-panel"} />
-        <Scrolling mode={scrollingMode} />
-        <Paging defaultPageSize={10} />
-        <Selection mode="multiple" allowSelectAll={false}/>
-        <ColumnFixing enabled={true} />
-        <Editing
-          mode="row"
-          allowDeleting={true}
-          allowUpdating={true}
-          useIcons={true}>
-            <Popup title="Employee Info" showTitle={true} width={700} height={525} />
+      {console.log("loading", loading)}
+      {/* {loading && <LoadPanel visible={true} />} */}
+      {loading ? (
+        <div>
+          <div style={{ display: "flex", justifyContent: "end" }}>
+            <Skeleton height={40} width={200} />
+          </div>
+          <Skeleton height={300} />
+        </div>
+      ) : (
+        <DataGrid
+          id="data-grid-container-local"
+          dataSource={transporterDataSource}
+          keyExpr={"srNo"}
+          showBorders={true}
+          focusedRowEnabled={true}
+          defaultFocusedRowIndex={0}
+          columnAutoWidth={true}
+          remoteOperations={true}
+          height={400}
+          className="gate-in-data-grid"
+        >
+          <SearchPanel visible={true} width={190} className={"search-panel"} />
+          <Scrolling mode={scrollingMode} />
+          <Paging defaultPageSize={10} />
+          <Selection mode="multiple" allowSelectAll={false} />
+          <ColumnFixing enabled={true} />
+          <Editing
+            mode="row"
+            allowDeleting={true}
+            allowUpdating={true}
+            useIcons={true}
+          >
+            <Popup
+              title="Employee Info"
+              showTitle={true}
+              width={700}
+              height={525}
+            />
           </Editing>
-        <Column type="buttons" caption="Actions">
-          <Button name="edit" />
-          <Button name="delete" />
-        </Column>
-        <Column
-          dataField={"docNum"}
-          caption={"PO Document No."}
-          allowEditing={false}
-          width={182}
-        />
-        <Column
-          dataField={"gateInNo"}
-          caption={"Gate In No."}
-          allowEditing={false}
-        />
+          <Column type="buttons" caption="Actions">
+            <Button name="edit" />
+            <Button name="delete" />
+          </Column>
+          <Column
+            dataField={"docNum"}
+            caption={"PO Document No."}
+            allowEditing={false}
+            width={182}
+          />
+          <Column
+            dataField={"gateInNo"}
+            caption={"Gate In No."}
+            allowEditing={false}
+          />
 
-        <Column
-          dataField={"recDate"}
-          caption={"Recieved Date"}
-          allowEditing={false}
-        />
-        <Column
-          dataField={"vehicleNo"}
-          caption={"Vehical No."}
-          allowEditing={false}
-        />
-        <Column
-          dataField={"transporterCode"}
-          caption={"Transporter Code"}
-          allowEditing={false}
-        />
-        
-      </DataGrid>
-      
+          <Column
+            dataField={"recDate"}
+            caption={"Recieved Date"}
+            allowEditing={false}
+          />
+          <Column
+            dataField={"vehicleNo"}
+            caption={"Vehical No."}
+            allowEditing={false}
+          />
+          <Column
+            dataField={"transporterCode"}
+            caption={"Transporter Code"}
+            allowEditing={false}
+          />
+        </DataGrid>
+      )}
+
       <div
         className="content-block-save content-block-wrapper"
         style={{ justifyContent: "flex-end", marginTop: "10rem" }}
